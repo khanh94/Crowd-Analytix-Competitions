50c6fd72a1b975cfe571f5de3a41c3bb49dbbe2b
khanh94 <knguyen1@colgate.edu>
2015-07-22 14:59:46 -0400

    Prediction Script

--- First prediction script.R
+++ First prediction script.R
@@ -7,8 +7,12 @@ cancer_test = read.csv('CAX_CANCER_TEST_data.csv')
 Regression = lm(TD50~Mol.Wt. + Volume + H.DonorCount + RotatableBondCount, data=cancer_train)
 
 #Make a prediction using the model
-first_prediction = predict(Regression, newdata=cancer_train)
+first_prediction = predict(Regression, newdata=cancer_test)
 
 #Calculate SSE & RMSE
-SSE = sum((first_prediction - cancer_train$TD50)^2)
-RMSE = sqrt(SSE/nrow(cancer_train))
+#SSE = sum((first_prediction - cancer_test$TD50)^2)
+#RMSE = sqrt(SSE/nrow(cancer_train))
+
+#Write to csv files
+submission$TD50 = first_prediction
+write.csv(submission, file="/Users/ChemLab6/Documents/Crowd-Analytix-Competitions/submission.csv")
